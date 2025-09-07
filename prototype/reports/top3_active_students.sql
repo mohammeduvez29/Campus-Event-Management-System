1c8aa9b4fa49dd7981de5ea52341bed68948ae2a
MOHAMMED UVEZ KHAN <116294061+mohammeduvez29@users.noreply.github.com>
2025-09-07 13:08:31 +0530

    Create top3_active_students.sql

---
+++ prototype/reports/top3_active_students.sql
@@ -0,0 +1,6 @@
+SELECT students.name, COUNT(a.id) AS attended_events
+FROM students
+LEFT JOIN attendance a ON students.id = a.student_id
+GROUP BY students.id
+ORDER BY attended_events DESC
+LIMIT 3;
